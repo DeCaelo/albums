5bb845f29eb9dbf5261e679f14a72e1141763b61
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2017-01-23 16:46:40 +0100

    Component level state

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -3,12 +3,18 @@ import { View, Text } from 'react-native';
 import axios from 'axios';
 
 class AlbumList extends Component {
+  // 1. initialize default state
+  state = { albums: [] };
+
   componentWillMount() {
     axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-      .then(response => console.log(response));
+    // 2. make sure after fetching your data, we take that state, and updating component state
+      .then(response => this.setState({ albums: response.data }));
   }
 
   render() {
+    console.log(this.state);
+
     return (
       <View>
         <Text>Album List</Text>
